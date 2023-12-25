bac9b75dfc10ceecdf065fe2ac50e01c31a642a2
xeneskus <xeneskus@gmail.com>
2023-12-25 10:21:25 +0300

    Application Layer Implementation

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -12,6 +12,7 @@ namespace Application
     {//extension method static olmalı, neyi extension ediyoruz this ile belirtiyoruz
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddMediatR(configuration =>
             {
                 configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());//git bütün assembly tara orada command handler bul onları birbirileriyle eşleştir listlee koy yarın bir gün send yaparsak onu çalıştır
